6daaed9f33a77e52ded55d4b74f73a0cb8e1a3a9
Christopher Leja <christopher.leja@gmail.com>
2020-02-17 21:40:20 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -8,20 +8,21 @@ def nyc_pigeon_organizer(data)
           hash[name][colors_gender_lives] = []
         else
           hash[name][colors_gender_lives] = []
+          
         end
-        
       end
     end
   end
+  
   hash.each do |name, values|
     values.each do |hashvalue, array|
       data.each do |colors_gender_lives, values|
         values.each do |value, array|
           array.each do |element|
-
+            
             if element == name && hashvalue == colors_gender_lives
-              
               hash[name][hashvalue] << value.to_s
+              
             end
           end
         end
